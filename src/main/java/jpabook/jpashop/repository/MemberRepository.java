0e092cc80b7203c25e9ad4d01b8a0f59be579266
조영혜 <0808yeonghye@gmail.com>
2022-05-05 23:44:53 +0900

    MemberRepository, MemberService 만들고 Annotation추가.

---
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,38 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Repository // 스프링 사용하는 거니까 스프링빈으로 자동관리됨.
+@RequiredArgsConstructor
+public class MemberRepository {
+
+    @PersistenceContext // jpa 사용하기 때문에 jpa가 제공하는 표준 어노테이션 명시.
+    private final EntityManager em; // 스프링이 엔티티매니저를 만들어서 주입해줌. 아래는 이제 저장로직 짜면됨.
+
+//    @PersistenceUnit // 엔티티매니저팩토리를 직접 주입하고 싶을 때 사용.
+//    private EntityManagerFactory emf;
+
+    public void save(Member member){
+        em.persist(member);
+    }
+    public Member findOne(Long id){
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll(){
+        List<Member> result = em.createQuery("select  m  from Member m", Member.class).getResultList();
+
+        return result;
+    }
+
+    public List<Member> findByName(String name){
+        return em.createQuery("select m from Member m where m.name = :name",Member.class)
+                .setParameter("name", name).getResultList();
+    }
+
+}
---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,51 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true) // 데이터 변경 할 때는 꼭 명시해야함.
+@RequiredArgsConstructor // final가지고 있는 필드만 생성자 만들어줌! -> 이거 썼으니까 아래 생성자 생성 코드 삭제.
+public class MemberService {
+
+    private final MemberRepository memberRepository; // 필드는 파이널로 해주는 걸 권장. -> 컴파일 시점에 체크 할 수 있음.
+
+//    @Autowired // setter 인젝션. 필드는 주입하기 좀 까다로운데 메소드로 진행하니까 테스트 돌릴 때 좋음.
+//    public void setMemberRepository(MemberRepository memberRepository){
+//        this.memberRepository = memberRepository;
+//    }
+
+//    @Autowired // 생성자 인젝션. 한번 생성할 때 완성이 되니까 중간에 set으로 변경 안됨. 멤버 서비스 개발할 떄 의존관계 명확하게 파악할 수 있음.
+//                // 생성자 하나일 땐 자동으로 생성자 인젝션 됨. @Autowired 명시 안해도됨.
+//    public MemberService(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+
+    // 회원가입
+    @Transactional
+    public Long join(Member member){
+        validateDuplicateMember(member); // 중복회원검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member){ // 멤버 수를 세서 그 멤버가 0보다 크면 문제가 있다. 이런식으로 하는 게 더 최적화
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()){
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+    // 회원 전체조회
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+    public Member findOne(Long memberId){
+        return memberRepository.findOne(memberId);
+    }
+}
